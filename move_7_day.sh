d8acc80e2cf162e30d62285cc30e1714864b9a81
Hugo Cosme <hugutux@gmail.com>
2018-11-03 01:44:45 -0300

    deletando coisas

--- move_7_day.sh
+++ move_7_day.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 #movendo arquivos com mais de sete dias
 find /home/hugo/Downloads/* -mtime +7 -exec mv {} /home/hugo/Downloads/teste  \;
-
-#find /var/www/carenet/app-stream-android/build_from_gulp -mtime +7 -exec mv {} /var/www/carenet/app-stream-android/build_from_gulp/bkp app  \;
