75db3f1a58d978435c13b2e6c8f857efc76439cf
Hugo Cosme <hugutux@gmail.com>
2018-11-03 00:43:42 -0300

    add como criar collection e inserir informações nela

--- conn_mongodb.sh
+++ conn_mongodb.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 #conectar no mongo pelo shell
 #principais comandos mongodb
+
+#entrar no mongo local sem senha;
+mongo
+
+#criando collection
+use usuarios
+
+#inserindo informação nessa collection
+db.usuarios.insert({ nome: "Hugo", idade: 30 })
