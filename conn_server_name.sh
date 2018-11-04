5b8b4feae535c519a56eee3c5f7055ecf1984eac
Hugo Cosme <hugutux@gmail.com>
2018-11-04 20:49:18 -0200

    conectando com o servidor pelo nome ou ip e buscando a chave, utilizando variaves criadas pelo read

---
+++ conn_server_name.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+
+#mudar a cor
+#setterm -foreground black -background green
+
+echo "Veja a lista de servidores e Ambientes.
+        Pode ter algo aqui..."
+        sleep 1
+echo
+echo
+        echo    Digite o Endereço:;
+        read -t30 server;
+
+echo
+        echo    Qual seu usuário?;
+echo
+        read -t20 user
+echo
+
+echo Iniciando a conexão em $server com o Usuário $user!
+        ssh $user@$server -i /home/$user/ssh/id_rsa
